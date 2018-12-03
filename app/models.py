ea0bc4c3aae0b8d149ca8c01ad34b36002a1fb14
MrVeshij <35579329+MrVeshij@users.noreply.github.com>
2018-12-03 23:22:04 +0300

    update some bd, add new model 'Tag'

--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ def slugify(text):
 	pattern = r'[^\w+]'
 	return re.sub(pattern, '-', text)
 
+
+posts_tags = db.Table('posts_tags',
+			 db.Column('post_id', db.Integer, db.ForeignKey('post_id')),
+			 db.Column('tag_id', db.Integer, db.ForeignKey('tag_id'))
+			 )
+
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(140))
@@ -21,3 +27,12 @@ class Post(db.Model):
 		self.slug = slugify(self.title)
 		
 
+class Tag(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(30))
+	slug = db.Column(db.String(30), unique=True)
+	created = db.Column(db.DateTime, default=datetime.now())
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.slug = slugify(self.title)
